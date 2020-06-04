8f294e59a3b8e505c18ba0c31bd6954e657d6fd0
xingyu711 <61847270+xingyu711@users.noreply.github.com>
2020-06-03 20:24:12 -0700

    display notes and adding a search box

--- src/components/Transactions/TransactionSheet.tsx
+++ src/components/Transactions/TransactionSheet.tsx
@@ -1,8 +1,9 @@
-import { Table, Popconfirm } from 'antd';
+import { Input, Table, Popconfirm } from 'antd';
 import React from 'react';
 import styles from './TransactionSheet.css';
 import moment from 'moment';
 
+const {Search} = Input;
 
 class TransactionSheet extends React.Component {
   componentDidMount() {
@@ -16,6 +17,7 @@ class TransactionSheet extends React.Component {
       title: 'Type',
       dataIndex: 'basic_type',
       key: 'basic_type',
+      width: '10%',
       filters: [
         { text: 'Income', value: 'income' },
         { text: 'Expense', value: 'expense' },
@@ -25,27 +27,36 @@ class TransactionSheet extends React.Component {
     {
       title: 'Category',
       dataIndex: 'category',
-      width: '20%',
+      width: '18%',
       key: 'category',
     },
     {
       title: 'Currency',
       dataIndex: 'currency',
       key: 'currency',
+      width: '12%',
       render: (value) => value.toUpperCase(),
     },
     {
       title: 'Amount',
       key: 'amount',
       dataIndex: 'amount',
+      width: '12%',
       sorter: (a, b) => a.amount - b.amount,
     },
     {
       title: 'Date',
       dataIndex: 'created',
       key: 'created',
+      width: '15%',
       render: (value) => moment(value).format('l'),
       sorter: (a, b) => moment(a.created).unix() - moment(b.created).unix(),
+    },
+    {
+      title: 'Note',
+      dataIndex: 'note',
+      key: 'note',
+      width: '23%',
     },
     {
       title: 'Actions',
@@ -82,18 +93,28 @@ class TransactionSheet extends React.Component {
 
   render() {
     return (
-      <Table
-        columns = {this.columns}
-        dataSource = {this.state.dataSource.map((entry, i) => ({...entry, key: entry.id}))}
-        className={styles.table}
-        expandable={{
-          expandedRowRender: record => <p>Note: {record.note}</p>,
-          rowExpandable: record => record.note !== null,
-        }}
-        scroll={{ y: 400}}
-        pagination={{ pageSize: 10}}
-        bordered
-      />
+      <div className={styles.container}>
+        <Search
+          placeholder="search transactions"
+          onSearch={value => console.log(value)}
+          enterButton
+          size="large"
+          className={styles.searchBox}
+        />
+        <Table
+          columns = {this.columns}
+          dataSource = {this.state.dataSource.map((entry, i) => ({...entry, key: entry.id}))}
+          className={styles.table}
+          // expandable={{
+          //   expandedRowRender: record => <p>Note: {record.note}</p>,
+          //   rowExpandable: record => record.note !== null,
+          // }}
+          scroll={{ y: 400}}
+          pagination={{ pageSize: 10}}
+          bordered
+        />
+      </div>
+      
     )
   }
 }
